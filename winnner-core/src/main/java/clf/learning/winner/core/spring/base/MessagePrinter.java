58ec8c65dbd2ba64cf144e43d93052c02338023b
chenlongfei <chenlongfei@corp.netease.com>
2017-11-01 19:42:42 +0800

    add

---
+++ winnner-core/src/main/java/clf/learning/winner/core/spring/base/MessagePrinter.java
@@ -0,0 +1,18 @@
+package clf.learning.winner.core.spring.base;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author chenlongfei
+  */
+@Component
+public class MessagePrinter {
+	
+	@Autowired
+	private MessageService messageService;
+	
+	public void printMessage() {
+		System.out.println(messageService.getMessage());
+	}
+}
---
+++ winnner-core/src/main/java/clf/learning/winner/core/spring/base/MessageService.java
@@ -0,0 +1,10 @@
+package clf.learning.winner.core.spring.base;
+
+/**
+ * @author chenlongfei
+ */
+public interface MessageService {
+	
+	String getMessage();
+	
+}
