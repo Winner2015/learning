50bd4ad2590dfc83dda3c882037cca79337eecc7
chenlongfei <chenlongfei@corp.netease.com>
2017-11-02 21:22:17 +0800

    addResourceHandlers

--- winner-web/src/main/java/clf/learning/winner/springboot/CustomWebMvcConfig.java
+++ winner-web/src/main/java/clf/learning/winner/springboot/CustomWebMvcConfig.java
@@ -1,6 +1,8 @@
 package clf.learning.winner.springboot;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -10,21 +12,29 @@ import clf.learning.winner.springboot.web.interceptor.AccessLogInterceptor;
 
 /**
  * @author chenlongfei
-  */
+ */
 @Configuration
+//@EnableWebMvc // = <mvc:annotation-driven/>
+// 不开启@EnableWebMvc时，Spring Boot使用 WebMvcAutoConfiguration
+// 中的各种配置；开启后，WebMvcAutoConfiguration失效
 public class CustomWebMvcConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		//添加拦截器
+		// 添加拦截器
 		registry.addInterceptor(new AccessLogInterceptor()).addPathPatterns("/**");
 		super.addInterceptors(registry);
 	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// TODO Auto-generated method stub
+		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/clf/images/");
 		super.addResourceHandlers(registry);
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
 	}
 
 	@Override
@@ -32,7 +42,5 @@ public class CustomWebMvcConfig extends WebMvcConfigurerAdapter {
 		// TODO Auto-generated method stub
 		super.addViewControllers(registry);
 	}
-	
-	
-	
+
 }
--- winner-web/src/main/java/clf/learning/winner/springboot/SpringBootApp.java
+++ winner-web/src/main/java/clf/learning/winner/springboot/SpringBootApp.java
@@ -2,12 +2,14 @@ package clf.learning.winner.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author chenlongfei
   */
 @SpringBootApplication // = @Configuration + @EnableAutoConfiguration + @ComponentScan
 //默认以@SpringBootApplication所在包为basePackage = @ComponentScan(basePackages = {"clf.learning.winner.springboot"})
+@Import(value= {})
 public class SpringBootApp {
 	
 	public static void main(String [] args) {
