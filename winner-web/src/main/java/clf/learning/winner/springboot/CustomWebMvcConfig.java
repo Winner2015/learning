54b71c9dd5de0a3e779c349abc2adcceeef59f24
chenlongfei <chenlongfei@corp.netease.com>
2017-11-03 11:10:51 +0800

    WebJarsController

--- winner-web/src/main/java/clf/learning/winner/springboot/CustomWebMvcConfig.java
+++ winner-web/src/main/java/clf/learning/winner/springboot/CustomWebMvcConfig.java
@@ -14,11 +14,11 @@ import clf.learning.winner.springboot.web.interceptor.AccessLogInterceptor;
  * @author chenlongfei
  */
 @Configuration
-//@EnableWebMvc // = <mvc:annotation-driven/>
-// 不开启@EnableWebMvc时，Spring Boot使用 WebMvcAutoConfiguration
-// 中的各种配置；开启后，WebMvcAutoConfiguration失效
+@EnableWebMvc // = <mvc:annotation-driven/>，不开启时，Spring Boot默认使用 WebMvcAutoConfiguration中的各种配置
 public class CustomWebMvcConfig extends WebMvcConfigurerAdapter {
 
+	//通过WebMvcConfigurerAdapter可以实现spring-mvc.xml的配置功能
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// 添加拦截器
@@ -28,10 +28,25 @@ public class CustomWebMvcConfig extends WebMvcConfigurerAdapter {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		
+		/**
+		 * 默认的静态资源处理器为：ResourceHttpRequestHandler 
+		 * 默认配置的 "/**" 映射到"/static"（或"/public"、"/resources"、"/META-INF/resources"），见ResourceProperties
+		 * 默认配置的 "/webjars/**" 映射到 "classpath:/META-INF/resources/webjars/", 见ResourceProperties
+		 * 例如,请求"/images/head.jpg"，会默认去"src/main/resources/static/images/head.jpg"下面寻找
+		 * 如果资源路径与默认不符，需要新增resourHandler来处理
+		 */
+		
+		//用户访问"/images/"下的静态资源时，会转到实际目录"/clf/images/"
 		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/clf/images/");
 		super.addResourceHandlers(registry);
+		
+		//还有一种全局配置方式，在 application.properties中添加：
+		//spring.mvc.static-path-pattern=
+		//spring.resources.static-locations=
 	}
 
+	//对静态资源的请求转发到容器缺省的servlet，而不使用DispatcherServlet
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
