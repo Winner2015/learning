c5e2f636a8df45fbb1aea1ce5015bfc2be194b0d
chenlongfei <496221132@qq.com>
2018-12-23 02:12:45 +0800

    bb

--- winner-web/src/main/java/clf/learning/test/Test.java
+++ winner-web/src/main/java/clf/learning/test/Test.java
@@ -1,7 +1,9 @@
 package clf.learning.test;
 
 public class Test {
-    public static void main(String [] args) {
+    public static void main(String args []) {
         System.out.println(11);
+
+
     }
 }
